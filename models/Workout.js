00a09c657cf0d19c79dedb7d902ec9e679f06aa3
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2021-01-05 19:17:08 -0500

    added schema

---
+++ models/Workout.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema(
+    {
+      day: {
+        type: Date,
+        default: Date.now,
+      },
+      exercises: [
+        {
+          type: {
+            type: String,
+          },
+          name: {
+            type: String,
+          },
+          duration: {
+            type: Number,
+          },
+          weight: {
+            type: Number,
+          },
+          reps: {
+            type: Number,
+          },
+          reps: {
+            type: Number,
+          },
+          sets: {
+            type: Number,
+          },
+          distance: {
+            type: Number,
+          },
+        },
+      ],
+      totalDuration: {
+        type: Number,
+        default: 0,
+      },
+    },
+    { toJSON: { virtuals: true } }
+  );
+  
+  const Workout = mongoose.model("Workout", WorkoutSchema);
+ 
+  module.exports = Workout;
