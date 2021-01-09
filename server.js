238b105189a3d50fa578b0f8d3952fe768f8ee5e
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2021-01-09 11:14:14 -0500

    added unifiedTopology and createIndex to mongoose.connect

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
